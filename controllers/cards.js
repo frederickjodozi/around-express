1e6a420c888c8187c8cdcefebae25e71aafd665c
Fred776 <fredfred776@gmail.com>
2022-03-10 15:28:26 -0500

    refactor controllers using promises

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,16 +1,14 @@
 const path = require('path');
 const CARDS_PATH = path.join(__dirname, '../data/cards.json');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const getCards = (req, res) => {
-  fs.readFile(CARDS_PATH, { encoding: 'utf8' }, (err, data) => {
-    if(err) {
-      res.send(err);
-      return
-    }
-    const cardsData = JSON.parse(data);
-    res.send(cardsData);
-  });
+  fs.readFile(CARDS_PATH, { encoding: 'utf8' })
+    .then(data => {
+      const cardsData = JSON.parse(data);
+      res.send(cardsData);
+    })
+    .catch(() => res.status(500).send({ Message: 'An error as occured on the server' }));
 }
 
 module.exports = getCards;
--- controllers/users.js
+++ controllers/users.js
@@ -1,32 +1,27 @@
 const path = require('path');
 const USERS_PATH = path.join(__dirname, '../data/users.json')
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const getUser = (req, res) => {
-  fs.readFile(USERS_PATH, { encoding: 'utf8' }, (err, data) => {
-    if(err) {
-      res.send(err)
-      return
-    }
-    const { id } = req.params;
-    const userData = JSON.parse(data);
-    const user = userData.find((user) => user._id === id);
-    if(!user) {
-      res.status(404).send({ message: "User ID not found" });
-    }
-    res.send(user);
-  });
+  fs.readFile(USERS_PATH, { encoding: 'utf8' })
+    .then(data => {
+      const { id } = req.params;
+      const userData = JSON.parse(data);
+      const user = userData.find(user => user._id === id);
+
+      if(!user) res.status(404).send({ message: "User ID not found" });
+      else res.send(user);
+    })
+    .catch(() => res.status(500).send({ Message: 'An error as occured on the server' }));
 }
 
 const getUsers = (req, res) => {
-  fs.readFile(USERS_PATH, { encoding: 'utf8' }, (err, data) => {
-    if(err) {
-      res.send(err);
-      return
-    }
-    const usersData = JSON.parse(data);
-    res.send(usersData);
-  });
+  fs.readFile(USERS_PATH, { encoding: 'utf8' })
+    .then(data => {
+      const usersData = JSON.parse(data);
+      res.send(usersData);
+    })
+    .catch(() => res.status(500).send({ Message: 'An error as occured on the server' }));
 }
 
 module.exports = { getUser, getUsers }

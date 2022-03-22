23c41790e58568c0532ab00597e03b9204b3f5ee
Fred776 <fredfred776@gmail.com>
2022-03-22 16:15:48 -0400

    add mongoose and connect with mongodb

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
-
+const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const routes = require('./routes/index');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(routes);
 app.listen(PORT, () => console.log(`We're live on ${PORT}!`));

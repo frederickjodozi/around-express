0e48c72dd0cc289185e99876cf94fce665b300bb
Fred776 <fredfred776@gmail.com>
2022-03-23 00:07:02 -0400

    use express.json()

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/aroundb');
 
+app.use(express.json());
 app.use(routes);
 app.listen(PORT, () => console.log(`We're live on ${PORT}!`));
 

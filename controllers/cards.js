c888535ce305bb03bfe13105f4e16b077fa80ae2
Fred776 <fredfred776@gmail.com>
2022-03-23 19:42:31 -0400

    connect card controllers with card model

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,25 @@
-const path = require('path');
-const CARDS_PATH = path.join(__dirname, '../data/cards.json');
-const readFile = require('../helpers/index');
+const Card = require('../models/cards');
 
 const getCards = (req, res) => {
-  readFile(CARDS_PATH)
+  Card.find({})
     .then(data => res.send(data))
     .catch(() => res.status(500).send({ errorMessage: 'An error as occured on the server' }));
 }
 
-module.exports = getCards;
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  Card.create({ name, link })
+    .then(data => res.send(data))
+    .catch(() => res.status(500).send({ errorMessage: 'An error as occured on the server' }));
+}
+
+const deleteCard = (req, res) => {
+  const { id } = req.body;
+
+  Card.deleteOne(id)
+  .then(data => res.send(data))
+  .catch(() => res.status(500).send({ errorMessage: 'An error as occured on the server' }));
+}
+
+module.exports = { getCards, createCard, deleteCard };
--- models/cards.js
+++ models/cards.js
@@ -25,11 +25,10 @@ const cardSchema = new mongoose.Schema({
   likes: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    default: []
   },
   createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
   }
 });
 
